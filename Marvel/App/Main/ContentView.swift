b59e5546b722039d71d2bd8c26e41bc5063d54ca
Nico <fohlen_autos_0e@icloud.com>
2024-04-27 18:58:49 +0200

    Initial Commit

---
+++ Marvel/App/Main/ContentView.swift
@@ -0,0 +1,25 @@
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Nico on 27.04.24.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Marvel/App/Main/MarvelApp.swift
@@ -0,0 +1,17 @@
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by Nico on 27.04.24.
+//  
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
